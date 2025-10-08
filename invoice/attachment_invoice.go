6103c0d3d663fa89b255396c3abdcd05b92d8eb2
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2025-10-08 13:39:19 +0300

    Update invoice attachment URL and remove list function
    - Change DeleteInvoiceFile URL from /attachment to /documents - Remove
    ListInvoiceAttachments function Update invoice document endpoints,
    remove list function

--- invoice/attachment_invoice.go
+++ invoice/attachment_invoice.go
@@ -40,7 +40,7 @@ func (c *API) AttachInvoiceFile(request interface{}, invoiceId string, file []by
 }
 func (c *API) DeleteInvoiceFile(request interface{}, invoiceId, attachID string) (data DeleteAttachmentResponse, err error) {
 	endpoint := zoho.Endpoint{
-		URL:    fmt.Sprintf("%s%s/%s/attachment/%s", InvoiceAPIEndpoint, InvoicesModule, invoiceId, attachID),
+		URL:    fmt.Sprintf("%s%s/%s/documents/%s", InvoiceAPIEndpoint, InvoicesModule, invoiceId, attachID),
 		Method: zoho.HTTPDelete,
 		URLParameters: map[string]zoho.Parameter{
 			"filter_by": "",
@@ -71,47 +71,4 @@ func (c *API) DeleteInvoiceFile(request interface{}, invoiceId, attachID string)
 type DeleteAttachmentResponse struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
-}
-
-func (c *API) ListInvoiceAttachments(invoiceId string) (data ListAttachmentResponse, err error) {
-
-	endpoint := zoho.Endpoint{
-		URL:    fmt.Sprintf("%s%s/%s/attachment", InvoiceAPIEndpoint, InvoicesModule, invoiceId),
-		Method: zoho.HTTPGet,
-		URLParameters: map[string]zoho.Parameter{
-			"filter_by": "",
-		},
-		BodyFormat: zoho.JSON_STRING,
-		Headers: map[string]string{
-			InvoiceAPIEndpointHeader: c.OrganizationID,
-		},
-		ResponseData: &ListAttachmentResponse{},
-	}
-
-	err = c.Zoho.HTTPRequest(&endpoint)
-	if err != nil {
-		return ListAttachmentResponse{}, fmt.Errorf("Failed to list invoice attachments: %s", err)
-	}
-
-	if v, ok := endpoint.ResponseData.(*ListAttachmentResponse); ok {
-		return *v, nil
-	}
-	return ListAttachmentResponse{}, fmt.Errorf("Data retrieved was not 'ListAttachmentResponse'")
-}
-
-type ListAttachmentResponse struct {
-	Code    int                  `json:"code"`
-	Message string               `json:"message"`
-	Data    []AttachmentResponse `json:"data"`
-}
-type AttachmentResponse struct {
-	ID          string          `json:"id"`
-	CreatedTime string          `json:"created_time"`
-	Owner       AttachmentOwner `json:"owner"`
-	ParentID    string          `json:"parent_id"`
-}
-type AttachmentOwner struct {
-	Name  string `json:"name"`
-	ID    string `json:"id"`
-	Email string `json:"email"`
 }
